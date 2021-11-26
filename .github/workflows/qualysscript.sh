ff671785a4c1b84f4b679dad555a7c4a8003eeb9
Kong Yew Chan <kchan@qualys.com>
2021-11-25 21:08:07 -0800

    test1

--- .github/workflows/qualysscript.sh
+++ .github/workflows/qualysscript.sh
@@ -9,17 +9,7 @@
 # PRODUCTS/SERVICES AS PROVIDED BY QUALYS.
 #
 ################################################################################
-cat << EOLICENSE
-################################################################################
-# LICENSE
-# =======
-#
-# THIS SCRIPT IS PROVIDED TO YOU "AS IS." TO THE EXTENT PERMITTED BY LAW,
-# QUALYS HEREBY DISCLAIMS ALL WARRANTIES AND LIABILITY FOR THE PROVISION OR
-# USE OF THIS SCRIPT. IN NO EVENT SHALL THESE SCRIPTS BE DEEMED TO BE SUPPORTED
-# PRODUCTS/SERVICES AS PROVIDED BY QUALYS.
-################################################################################
-EOLICENSE
+echo "test1"
 
 #set -e
 set -x
