e9e27c6e57eea7a6301d8b5d536131f6d52235e2
Kong Yew Chan <kchan@qualys.com>
2021-11-25 21:02:14 -0800

    test1

--- .github/workflows/qualysscript.sh
+++ .github/workflows/qualysscript.sh
@@ -38,6 +38,7 @@ PASSWORD=$3
 IMAGE=$4
 
 check_command_exists () {
+    echo "This script requires $1 but it's not installed. Aborting."
 	hash $1 2>/dev/null || { echo >&2 "This script requires $1 but it's not installed. Aborting."; exit 1; }
 }
 
