2960496f3808a010f3b7225a56a0ce532b97ccfa
Kong Yew Chan <kchan@qualys.com>
2021-11-25 21:10:41 -0800

    test1

---
+++ .github/workflows/script1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+################################################################################
+# LICENSE
+# =======
+#
+# THIS SCRIPT IS PROVIDED TO YOU "AS IS." TO THE EXTENT PERMITTED BY LAW, 
+# QUALYS HEREBY DISCLAIMS ALL WARRANTIES AND LIABILITY FOR THE PROVISION OR 
+# USE OF THIS SCRIPT. IN NO EVENT SHALL THESE SCRIPTS BE DEEMED TO BE SUPPORTED 
+# PRODUCTS/SERVICES AS PROVIDED BY QUALYS.
+#
+################################################################################
+echo "test1"
